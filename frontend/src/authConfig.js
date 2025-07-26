3b887e410832976a4daac7fc220a9e4326bdcd1b
AdamDIOM <github@adamd.im>
2025-07-26 20:01:48 +0100

    update authconfig.js to have prod url

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,8 +1,12 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 export const msalConfig = {
     auth: {
         clientId: "b0ce37b8-4872-4f7d-b249-af7448c5db35",
         authority: "https://login.microsoftonline.com/5eb26f0a-532d-45f6-b1b4-58c84e52a7c5",
-        redirectUri: "http://localhost:5173/",
+        redirectUri: isLocalhost
+        ? "http://localhost:5173/"
+        : "https://assetsv2.netlify.app/",
         navigateToLoginRequestUrl: false,
     },
 //     system: {
