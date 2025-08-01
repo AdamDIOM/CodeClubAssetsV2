af8f10a26bdec1196dd281bf2e6dffc327474226
AdamDIOM <github@adamd.im>
2025-08-01 02:14:40 +0100

    added ux changes to list, added filtering, updated to database v2

--- backend/src/functions/getAssets.js
+++ backend/src/functions/getAssets.js
@@ -1,6 +1,21 @@
 const { app } = require('@azure/functions');
+const { OnBehalfOfCredential } = require('@azure/identity');
+const { ConfidentialClientApp } = require('@azure/msal-node');
 const sql = require('mssql');
 const dbConfig = require('../../dbConfig');
+
+async function getSqlAccessToken(userAccessToken) {
+    const credential = new OnBehalfOfCredential({
+        tenantId: '5eb26f0a-532d-45f6-b1b4-58c84e52a7c5',
+        clientId: '3aa70f4c-6909-497b-b2d1-b72b8a7aa70a',
+        clientSecret: process.env.CLIENT_SECRET,
+        userAssertionToken: userAccessToken
+    });
+
+    const token = await credential.getToken('https://database.windows.net/.default')
+    return token.token;
+}
+
 
 app.http('getAssets', {
     methods: ['GET'],
@@ -9,14 +24,40 @@ app.http('getAssets', {
         context.log(`getAssets called at "${request.url}"`);
 
         const searchTerm = request.query.get('f') || '';
-        console.log(searchTerm)
+        context.log(request.query)
+        context.log(searchTerm)
+
+        const authHeader = request.headers.get('Authorization')
+
+        if(!authHeader.startsWith('Bearer ')) {
+            return {status:401, body: "Missing or invalid Authorization header"}
+        }
+
+        const userAccessToken = authHeader.split(' ')[1]
 
         try {
-            const pool = await sql.connect(dbConfig);
+            const sqlAccessToken = await getSqlAccessToken(userAccessToken);
+
+            const pool = new sql.ConnectionPool({
+                server: process.env.DB_SERVER,
+                database: process.env.DB_NAME,
+                authentication: {
+                    type: 'azure-active-directory-access-token',
+                    options: {
+                        token: sqlAccessToken
+                    }
+                },
+                options: {
+                    encrypt: true
+                }
+            });
+
+            await pool.connect();
             const result = await pool.request()
                 .input('searchTerm', sql.NVarChar, `%${searchTerm}%`)
                 .query('SELECT * FROM dbo.Assets WHERE Name LIKE @searchTerm');
             const assets = result.recordset;
+            //context.log(result)
         
             return { 
                 status: 200,
@@ -24,7 +65,7 @@ app.http('getAssets', {
                 body: JSON.stringify(assets)
             };
         } catch (err) {
-            context.log.error('Database error: ', err);
+            context.error('Database error: ', err);
             return {
                 status: 500,
                 body: "Failed to retrieve assets from database."
